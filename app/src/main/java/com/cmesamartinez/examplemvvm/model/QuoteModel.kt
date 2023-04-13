81f3731989a327530556d26b6c06cf65453aa922
c.mesa.martinez <carolinamesamartinez1@gmail.com>
2023-04-13 17:39:44 +0200

    create a alication that send you a random quote when you press the screen with  MVVM

---
+++ app/src/main/java/com/cmesamartinez/examplemvvm/model/QuoteModel.kt
@@ -0,0 +1,4 @@
+package com.cmesamartinez.examplemvvm.model
+
+data class QuoteModel (val quote:String,val author:String){
+}
---
+++ app/src/main/java/com/cmesamartinez/examplemvvm/view/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.cmesamartinez.examplemvvm.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import com.cmesamartinez.examplemvvm.databinding.ActivityMainBinding
+import com.cmesamartinez.examplemvvm.viewModel.QuoteViewModel
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val quoteViewModel:QuoteViewModel by viewModels()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        quoteViewModel.quoteModel.observe(this, Observer {currentQuote->
+            binding.tvQuote.text=currentQuote.quote
+            binding.tvAuthor.text=currentQuote.author
+        })
+
+        binding.viewContainer.setOnClickListener {
+            quoteViewModel.randomQuote()
+        }
+    }
+}
